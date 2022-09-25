6978c387fc7bedc6509bb737215df973dab3973b
DeaWahsa <deawahsasaputri@gmail.com>
2022-09-25 20:10:08 +0800

    commit pertama

---
+++ FormProgress.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mentoring Asset</title>
+    <link rel="stylesheet" href="styleform.css">
+</head>
+
+<body>
+    <nav>
+        <div class="wrapper">
+            <a class="navbar-brand" href="#"><img src="/MentoringAsset/images/logo.png" alt="Lights" style="width:12%"></a>
+            <div class="menu">
+                <ul>
+                    <li><a href="" class="tbl-biru">Back</a></li>
+                    <li><a href="" class="tbl-biru">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <!-- untuk home -->
+        <section id="home">
+            <div class="kolom">
+                <h2>PROGRESS PENANGANAN PERMASALAHAN ASET TELKOM</h2>
+            </div>
+        </section>
+
+        <!-- untuk form -->
+
+        <div class="wrapper">
+            <div class="form">
+                <div class="inputfield">
+                    <label>Unit</label>
+                    <input type="text" class="input">
+                </div>
+                <div class="inputfield">
+                    <label>Penanggung Jawab</label>
+                    <input type="text" class="input">
+                </div>
+                <div class="inputfield">
+                    <label>Periode Monitoring</label>
+                    <input type="password" class="input">
+                </div>
+                <div class="inputfield">
+                    <input type="submit" value="Register" class="btn">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ laporan.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mentoring Asset</title>
+    <link rel="stylesheet" href="styledokumen.css">
+</head>
+
+<body>
+    <nav>
+        <div class="wrapper">
+            <a class="navbar-brand" href="#"><img src="/MentoringAsset/images/logo.png" alt="Lights" style="width:12%"></a>
+            <div class="menu">
+                <ul>
+                    <li><a href="" class="tbl-biru">Back</a></li>
+                    <li><a href="" class="tbl-biru">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <!-- untuk home -->
+        <section id="home">
+            <div class="kolom">
+                <h2>LAPORAN ASSET</h2>
+            </div>
+        </section>
+
+        <!-- untuk dokumen -->
+
+        <section id="dokumen">
+            <div class="laporan">
+                <div class="laporan-list">
+                    <div class="kartu-laporan">
+                        <p><a href="">LAPORAN PROGRESS PENANGANAN PERMASALAHAN ASET TELKOM TAHUN 2019</a></p>
+                    </div>
+                    <div class="kartu-laporan">
+                        <p><a href="">LAPORAN PROGRESS PENANGANAN PERMASALAHAN ASET TELKOM TAHUN 2020</a></p>
+                    </div>
+                    <div class="kartu-laporan">
+                        <p><a href="">LAPORAN PROGRESS PENANGANAN PERMASALAHAN ASET TELKOM TAHUN 2021</a></p>
+                    </div>
+                    <div class="kartu-laporan">
+                        <p><a href="">LAPORAN PROGRESS PENANGANAN PERMASALAHAN ASET TELKOM TAHUN 2022</a></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+</body>
+
+</html>
